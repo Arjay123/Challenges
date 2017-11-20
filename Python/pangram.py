6caaf8188831ba860e42fad3246206534c09602f
arjaynguyen <arjaynguyen123@hotmail.com>
2017-11-20 13:43:09 -0800

    pangrams explanation

--- Python/pangram.py
+++ Python/pangram.py
@@ -1,5 +1,12 @@
 # https://www.hackerrank.com/challenges/pangrams/problem
 
+"""
+Because the characters of the string s are constrained to spaces or alphabetic
+letters, we can simply create a set from a lowercase version of the string s.
+
+Because of the properties of a set, we can check if the length of the set
+equals the length of a string containing all the letters of the alphabet (26).
+"""
 from string import lowercase as LOWERCASE
 
 s = raw_input()
